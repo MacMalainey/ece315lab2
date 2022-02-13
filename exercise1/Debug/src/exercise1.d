c6e2d10ff22f9cebd2bb61db19a191fc50006a10
Mackenzie Malainey <macmalainey@gmail.com>
2022-02-13 15:53:05 -0700

    exercise 1 complete

---
+++ exercise1/Debug/src/exercise1.d
@@ -0,0 +1,99 @@
+src/exercise1.o src/exercise1.o: ../src/exercise1.c \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xuartps_hw.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/task.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/list.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/queue.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/task.h ../src/morseTranslator.h
+
+../../lab2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/task.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/list.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/queue.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/task.h:
+
+../src/morseTranslator.h:
---
+++ exercise2/Debug/src/exercise2.d
@@ -0,0 +1,55 @@
+src/exercise2.o src/exercise2.o: ../src/exercise2.c \
+ ../../lab2_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/task.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/list.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/queue.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/task.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/timers.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../lab2_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../../lab2_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/task.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/list.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/queue.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/task.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/timers.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../lab2_bsp/ps7_cortexa9_0/include/xparameters.h:
